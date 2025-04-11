62d7fcc2a8e4bb22b7f58ecb20e9da7f1e0caff6
K <155929866+kavinjs21@users.noreply.github.com>
2025-04-11 08:54:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-var toggleitm=document.querySelector(".navbar-toggle-itm");
-function menu(){
-    toggleitm.style.left="0";
-}
-function menuclose(){
-    toggleitm.style.left="-60%";
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const mobileMenu = document.querySelector(".navbar-toggle-itm");
+  const menuBtn = document.querySelector(".navbar-toggle");
+  const closeBtn = document.querySelector(".cross");
+
+  menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.add("active");
+  });
+
+  closeBtn.addEventListener("click", () => {
+    mobileMenu.classList.remove("active");
+  });
+});
